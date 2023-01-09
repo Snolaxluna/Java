fb3769fb5244c4861f98226fd4b819af40d979d2
Snolaxlucas <lucassolanich1987@gmail.com>
2023-01-08 23:40:23 -0300

    Conversao de tempo

---
+++ Java/Exercicios/src/ConverseTempo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ConverseTempo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int horas = N / 3600;
+        int minutos = (N % 3600) / 60;
+        int segundos = (N % 3600) % 60;
+
+        System.out.printf("%d:%d:%d\n", horas, minutos, segundos);
+
+    }
+}
