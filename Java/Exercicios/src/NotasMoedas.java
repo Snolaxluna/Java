a1d0d8f54f36d2478be949c6cd08b43104f710c6
Snolaxlucas <lucassolanich1987@gmail.com>
2023-02-04 13:17:11 -0300

    NotasMoedas final

--- Java/Exercicios/src/NotasMoedas.java
+++ Java/Exercicios/src/NotasMoedas.java
@@ -1,28 +1,68 @@
 import java.util.Scanner;
 
-public class NotasMoedas{
+public class NotasMoedas {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double num = sc.nextDouble();
 
-        double cem = num / 100;
-        double cinquenta = (num % 100) / 50;
-        double vinte = ((num %100) % 50) / 20;
-        double dez = (((num % 100) % 50) % 20) / 10;
-        double cinco = ((((num % 100) % 50) % 20) % 10) / 5;
-        double dois = (((((num % 100) % 50) % 20) % 10) % 5) / 2;
-        
-        double um = ((((((num % 100) % 50) % 20) % 10) % 5) % 2) / 1;
-        double zeroCinquenta = (((((((num % 100) % 50) % 20) % 10) % 5) % 2) % 1) / 0.50;
-        double zeroVintecinco= ((((((((num % 100) % 50) % 20) % 10) % 5) % 2) % 1) % 0.50) / 0.25;
-        double zeroDez = (((((((((num % 100) % 50) % 20) % 10) % 5) % 2) % 1) % 0.50) % 0.25) / 0.10;
-        double zeroCinco = ((((((((((num % 100) % 50) % 20) % 10) % 5) % 2) % 1) % 0.50) % 0.25) % 0.10) / 0.05;
-        double zeroUm = (((((((((((num % 100) % 50) % 20) % 10) % 5) % 2) % 1) % 0.50) % 0.25) % 0.10) % 0.05) / 0.01;
+        Scanner scanner = new Scanner(System.in);
+        double valor = scanner.nextDouble();
 
-        System.out.printf("%d notas (s) de R$ 100.00", cem);
-        
-        
+        double total, cem, cinquenta, vinte, dez, cinco, dois, umReal,
+                cinquentaCentavos, vinteCincoCentavos, dezCentavos, cincoCentavos, umCentavo;
 
-        sc.close();
+        cem = valor / 100;
+        total = valor % 100;
+
+        cinquenta = total / 50;
+        total = total % 50;
+
+        vinte = total / 20;
+        total = total % 20;
+
+        dez = total / 10;
+        total = total % 10;
+
+        cinco = total / 5;
+        total = total % 5;
+
+        dois = total / 2;
+        total = total % 2;
+
+        umReal = total;
+        total = total % 1;
+
+        cinquentaCentavos = total / 0.50;
+        total = total % 0.50;
+
+        vinteCincoCentavos = total / 0.25;
+        total = total % 0.25;
+
+        dezCentavos = total / 0.10;
+        total = total % 0.10;
+
+        cincoCentavos = total / 0.05;
+        total = total % 0.05;
+
+        umCentavo = total / 0.01;
+        total = total % 0.01;
+
+        System.out.println("NOTAS:");
+        System.out.println((int) cem + " nota(s) de R$ 100.00");
+        System.out.println((int) cinquenta + " nota(s) de R$ 50.00");
+        System.out.println((int) vinte + " nota(s) de R$ 20.00");
+        System.out.println((int) dez + " nota(s) de R$ 10.00");
+        System.out.println((int) cinco + " nota(s) de R$ 5.00");
+        System.out.println((int) dois + " nota(s) de R$ 2.00");
+
+        System.out.println("MOEDAS:");
+        System.out.println((int) umReal + " moeda(s) de R$ 1.00");
+        System.out.println((int) cinquentaCentavos + " moeda(s) de R$ 0.50");
+        System.out.println((int) vinteCincoCentavos + " moeda(s) de R$ 0.25");
+        System.out.println((int) dezCentavos + " moeda(s) de R$ 0.10");
+        System.out.println((int) cincoCentavos + " moeda(s) de R$ 0.05");
+        System.out.printf("%.0f moeda(s) de R$ 0.01\n", umCentavo);
+
+        scanner.close();
+
     }
 }
